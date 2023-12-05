b8f69310bf33e775dd4a8865f083c5f5f41996e0
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-12-05 17:47:29 +0200

    Exercise 10.10

--- rate-repository-app/src/theme.js
+++ rate-repository-app/src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textHeader: "white",
@@ -11,7 +13,11 @@ const theme = {
     navheading: 18,
   },
   fonts: {
-    main: "System",
+    main: Platform.select({
+      android: "Roboto",
+      ios: "Arial",
+      default: "System",
+    }),
   },
   fontWeights: {
     normal: "400",
