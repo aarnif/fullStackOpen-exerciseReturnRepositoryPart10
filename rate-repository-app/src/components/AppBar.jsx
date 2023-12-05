2fe6c300e2e8f20266e3ded6fac738c300f667ad
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-12-05 15:10:13 +0200

    Exercise 10.6

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,28 +1,50 @@
 import { View, StyleSheet, Pressable } from "react-native";
+import { Link } from "react-router-native";
 import Text from "./Text";
 
 const styles = StyleSheet.create({
-  container: {
+  navContainer: {
     paddingTop: 40,
     paddingLeft: 10,
     paddingBottom: 20,
     backgroundColor: "#24292e",
+    display: "flex",
+    flexDirection: "row",
   },
-  // ...
+  navItem: {
+    paddingRight: 10,
+  },
 });
 
-const onPressFunction = () => {
+const onPressRepos = () => {
   console.log("Repositories pressed!");
+};
+
+const onPressSignIn = () => {
+  console.log("Sign in pressed!");
 };
 
 const AppBar = () => {
   return (
-    <View style={styles.container}>
-      <Pressable onPress={onPressFunction}>
-        <Text color={"header"} fontWeight={"bold"} fontSize={"navheading"}>
-          Repositories
-        </Text>
-      </Pressable>
+    <View style={styles.navContainer}>
+      <View style={styles.navItem}>
+        <Link to="/">
+          <Pressable onPress={onPressRepos}>
+            <Text color={"header"} fontWeight={"bold"} fontSize={"navheading"}>
+              Repositories
+            </Text>
+          </Pressable>
+        </Link>
+      </View>
+      <View style={styles.navItem}>
+        <Link to="/signin">
+          <Pressable onPress={onPressSignIn}>
+            <Text color={"header"} fontWeight={"bold"} fontSize={"navheading"}>
+              Sign in
+            </Text>
+          </Pressable>
+        </Link>
+      </View>
     </View>
   );
 };
